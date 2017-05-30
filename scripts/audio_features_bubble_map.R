91c7264bc8fd5dd7d3a083a5681ffedc1e7c58c0
Joy Phillips <jlxp@uw.edu>
2017-05-29 22:35:17 -0700

    Joy's code updated

---
+++ scripts/audio_features_bubble_map.R
@@ -0,0 +1,56 @@
+library(devtools)
+library(jsonlite)
+library(httr)
+library(dplyr)
+library(plotly)
+library(RColorBrewer)
+
+#This is all the stuff spotify needs to know before you start
+spotifyEndpoint <- oauth_endpoint(NULL, "https://accounts.spotify.com/authorize", "https://accounts.spotify.com/api/token")
+spotifyApp <- oauth_app("spotify", '87ccb0dca2bc4cac82d82a731fa65295', '5094f0bd6d4b4a368a990909d2a15acd')
+spotifyToken <- oauth2.0_token(spotifyEndpoint, spotifyApp)
+
+# search for artist
+
+# function: artist name input
+FindArtistId <- function(name.of.artist) {
+  url.search.name <- paste0("https://api.spotify.com", "/v1/search?q=", name.of.artist, "&type=artist")
+  response <- GET(url.search.name, spotifyToken)
+  search.results <- fromJSON(toJSON(content(response)))
+  ids <- search.results$artists$items$id
+  return(ids[1])
+}
+# end function
+
+# take in artist ID and give the top songs with popularity
+
+top.10.songs <- function(input.ID) {
+  artist.get.url <- paste0("https://api.spotify.com", "/v1/artists/", input.ID, "/top-tracks?country=US")
+  response.artist <- GET(artist.get.url, spotifyToken)
+  artist.results <- fromJSON(toJSON(content(response.artist)))
+  top.ten <- select(artist.results$tracks, name, id, popularity, duration_ms)
+  return(top.ten)
+  #return(artist.results$tracks)
+}
+
+GetFeatures <- function(track.id) {
+  uri <- paste0("https://api.spotify.com/v1/audio-features/", track.id)
+  response <- GET(uri, spotifyToken)
+  results <- fromJSON(toJSON(content(response)))
+}
+
+tracks <- top.10.songs(FindArtistId("david+bowie"))
+
+temp <- data.frame(matrix(unlist(lapply(tracks$id, GetFeatures)), nrow = 10, byrow = 1))
+colnames(temp) <- c("danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "type", "id", "uri", "track_href", "analysis_url", "duration_ms", "time_signature")
+merged.tracks.features <- cbind(select(tracks, name, popularity), temp)
+
+
+plot_ly(merged.tracks.features, x = ~name, y = ~popularity, text = ~name, type = 'scatter', mode = 'markers', 
+        size = ~duration_ms/1000/60, color = as.factor(merged.tracks.features$popularity),
+        colors = brewer.pal(9,"Set1"),
+        marker = list(opacity = 0.8, sizemode = 'diameter', sizeref=3)) %>%
+  layout(title = 'Artist Name',
+         xaxis = list(showgrid = FALSE, showticklabels=TRUE, title="", type = "category"),
+         yaxis = list(showgrid = FALSE, showticklabels=FALSE, title="", type = "category"),
+         showlegend = FALSE)
