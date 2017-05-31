3ac000188ebd612245560a0054f6d78f4f0e1c5d
Mary Elizabeth Ward <mew34@uw.edu>
2017-05-31 15:29:49 -0700

    Added my shiny stuff

--- server.R
+++ server.R
@@ -6,6 +6,14 @@ shinyServer(function(input, output) {
   output$plot.value <- renderPlotly({
     return(CreateGraph({input$user.id}))
   })
-  
   output$value <- renderText({GetDuration(input$user.id)})
+ 
+   output$top.50 <- renderPlotly({
+    return(plot_ly(official.playlist, x = ~track.name, y = ~track.popularity, type = "scatter", hoverinfo = "text",
+                   text = ~paste("Song: ", unlist(track.name), "</br> Artist: ", track.album.artists, 
+                                 "</br> Popularity: ", unlist(track.popularity), sep = "")) %>%  
+             layout(xaxis = list(showticklabels = FALSE, title = "Your Songs"), 
+                    yaxis = list(title = "Popularity"),
+                    title = "The Popularity of the Current Top 50 Songs on Spotify"))})
+  
 })
--- ui.R
+++ ui.R
@@ -7,10 +7,16 @@ shinyUI(navbarPage(
       textInput('user.id', "Enter the user id", '1295238919')
     ),
     mainPanel(
+      tabsetPanel(
       #img(src = 'test.png', align = "right"),
-      h4("Please be patient, it takes about 20 seconds to generate the graph"),
-      plotlyOutput('plot.value'),
-      textOutput("value")
+        tabPanel(h4("Please be patient, it takes about 20 seconds to generate the graph"), 
+                 # Change this to say the title of the chart and add this statement somewhere else
+                plotlyOutput('plot.value'),
+                textOutput("value")
+        ),
+        tabPanel(h4("Top 50 Ranking Graph"),
+                 plotlyOutput('top.50'))
+      )
     )
   )
 ))
