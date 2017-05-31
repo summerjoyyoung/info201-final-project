864eedefa8a219f8a3ffcac48f2d85644ba37d10
Mary Elizabeth Ward <mew34@uw.edu>
2017-05-30 17:16:00 -0700

    made a chart

--- scripts/top_listened.R
+++ scripts/top_listened.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(httr)
 library(jsonlite)
 library(devtools)
+library(plotly)
 
 spotifyEndpoint <- oauth_endpoint(NULL, "https://accounts.spotify.com/authorize", "https://accounts.spotify.com/api/token")
 spotifyApp <- oauth_app("spotify", '87ccb0dca2bc4cac82d82a731fa65295', '5094f0bd6d4b4a368a990909d2a15acd')
@@ -12,7 +13,7 @@ spotifyToken <- oauth2.0_token(spotifyEndpoint, spotifyApp)
 spotifyUser <- 'spotify' 
 spotifyPlaylist <- '37i9dQZF1DXcBWIGoYBM5M'
 
-# Getitng the songs from the top 50 playlist
+# Getting the songs from the top 50 playlist
 songs.URL <- paste("https://api.spotify.com/v1/users/", spotifyUser, "/playlists/", spotifyPlaylist, "/tracks", sep = "")
 get.Songs <- GET(songs.URL, spotifyToken)
 all.playlist <- jsonlite::fromJSON(toJSON(content(get.Songs)))
@@ -21,18 +22,26 @@ all.playlist.flat <- flatten(all.playlist$items)
 
 # Creates the dataframe that has the information from the top 50 playlist.
 flat.playlist <- flatten(all.playlist$items) %>% 
-                 select(track.name, track.album.name, track.explicit, track.popularity, track.id, track.album.album_type)  
+                 select(track.name, track.album.name, track.explicit, track.popularity, track.album.album_type)  
 
 GetArtist <- function(artist.list){
-  artists <- unlist(artist.list)[4]
+  artists <- artist.list[1,4]
   return(artists)
 }
 artists <- lapply(all.playlist.flat$track.artists, GetArtist)
-flat.artists <- unlist(artists)
+track.album.artists <- unlist(artists)
+
+df.artists <- as.data.frame(track.album.artists)
 
 # Adds the artist name to the dataframe I had created above
-flat.playlist$track.artist.name <- flat.artists
-
+official.playlist <- cbind(flat.playlist, df.artists)
 
 # Making a chart that shows information based on popularity
-
+top.50.graph <- plot_ly(official.playlist, x = ~track.name, y = ~track.popularity, 
+                        color = ~track.name,
+                        type = "scatter", hoverinfo = "text", 
+                        text = ~paste("Song: ", unlist(track.name), "</br> Artist: ", track.album.artists, 
+                                      "</br> Popularity: ", unlist(track.popularity), sep = "")) %>%  
+                        layout(xaxis = list(showticklabels = FALSE, title = "Ranking"), 
+                               yaxis = list(title = "Popularity"),
+                        title = "Popularity of the Current Top 50 Songs on Spotify")
