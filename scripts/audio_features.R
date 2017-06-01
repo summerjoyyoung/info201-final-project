a208461ba0fb5732be397682e1786254a2a170ac
Joy Phillips <jlxp@uw.edu>
2017-05-31 19:54:13 -0700

    Joy added markers to her graph

--- scripts/audio_features.R
+++ scripts/audio_features.R
@@ -14,7 +14,6 @@ library(jsonlite)
 library(httr)
 library(dplyr)
 library(plotly)
-library(RColorBrewer)
 
 spotifyEndpoint <- oauth_endpoint(NULL, "https://accounts.spotify.com/authorize", "https://accounts.spotify.com/api/token")
 spotifyApp <- oauth_app("spotify", '87ccb0dca2bc4cac82d82a731fa65295', '5094f0bd6d4b4a368a990909d2a15acd')
@@ -35,43 +34,59 @@ JoysGraph <- function(spotifyPlaylist) {
   songs.URL <- paste0("https://api.spotify.com/v1/users/", spotifyUser, "/playlists/", spotifyPlaylist, "/tracks?fields=items(track)")
   get.Songs <- GET(songs.URL, spotifyToken)
   all.playlist <- jsonlite::fromJSON(toJSON(content(get.Songs)))
-  all.songs <- flatten(all.playlist$items)
-  #  Creates the dataframe that has the information from the top 50 playlist.
-    flat.playlist <- flatten(all.playlist$items) %>% 
-      select(track.name, track.album.name, track.explicit, track.popularity, track.id, track.album.album_type)  
+  all.playlist.flat <- flatten(all.playlist$items)
+
+  GetArtist <- function(artist.list){
+    artists <- artist.list[1,4]
+    return(artists)
+  }
   
+  artists <- lapply(all.playlist.flat$track.artists, GetArtist)
+  track.album.artists <- unlist(artists)
+  
+  df.artists <- as.data.frame(track.album.artists)
+  
+  # Adds the artist name to the dataframe I had created above
+  official.playlist <- cbind(flat.playlist, df.artists)
+  # End of Mary's Code
   
   # This line unlists the audio features and puts them into a data frame
-  playlist.audio.features <- data.frame(matrix(unlist(lapply(flat.playlist$track.id, GetFeatures)), nrow = nrow(flat.playlist), byrow = 1))
+  playlist.audio.features <- data.frame(matrix(unlist(lapply(official.playlist$track.id, GetFeatures)), nrow = nrow(flat.playlist), byrow = 1))
   # This line changes the data frame column names
   colnames(playlist.audio.features) <- c("danceability", 
-                                        "energy", 
-                                        "key", 
-                                        "loudness", 
-                                        "mode", 
-                                        "speechiness", 
-                                        "acousticness", 
-                                        "instrumentalness", 
-                                        "liveness", 
-                                        "valence", 
-                                        "tempo", 
-                                        "type", 
-                                        "id", 
-                                        "uri", 
-                                        "track_href", 
-                                        "analysis_url", 
-                                        "duration_ms", 
-                                        "time_signature")
+                                         "energy", 
+                                         "key", 
+                                         "loudness", 
+                                         "mode", 
+                                         "speechiness", 
+                                         "acousticness", 
+                                         "instrumentalness", 
+                                         "liveness", 
+                                         "valence", 
+                                         "tempo", 
+                                         "type", 
+                                         "id", 
+                                         "uri", 
+                                         "track_href", 
+                                         "analysis_url", 
+                                         "duration_ms", 
+                                         "time_signature")
   # This line binds the track name and popularity columns to the playlist audio features
-  playlist.audio.features <- cbind(select(flat.playlist, track.name, track.popularity), playlist.audio.features)
+  playlist.audio.features <- cbind(official.playlist, playlist.audio.features)
   
   
   # This creates a scatter plot of track name by whatever audio features variable the user selects.
   # In the UI file I was thinking there would be a dropdown menu or radio buttons that the user can
   # select to change what variable is graphed on the y axis.
   
-  p <- playlist.audio.features %>% plot_ly(x = ~track.name, y = ~danceability, type = 'scatter', mode = 'markers') %>%
-    layout(xaxis = list(showticklabels = FALSE, title = "Track Names"), yaxis = list(title = "Danceability"))
+  p <- playlist.audio.features %>% plot_ly(x = ~track.name, 
+                                           y = ~danceability, 
+                                           type = 'scatter', 
+                                           mode = 'markers', 
+                                           text = ~paste0("Track Name: ", track.name, "\nArtist: ", track.album.artists, "\nTrack Popularity: ", track.popularity)) %>%
+    layout(xaxis = list(showticklabels = FALSE, title = "Track Names"), 
+           yaxis = list(title = "Danceability")
+           )
   return(p)
 }
 
